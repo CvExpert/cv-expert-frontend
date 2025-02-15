9c607826498a5dadaa92926488f4b1d36fe03079
Abhishek Kumar <abhishek.nitdelhi@gmail.com>
2025-02-16 03:11:41 +0530

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,60 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import DefaultLayout from "@/layouts/default";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <span className={title()}>Optimise your Resume with &nbsp;</span>
+          <span className={title({ color: "violet" })}>AI </span>
+          <span className={title()}>
+          & Land Your Dream Job Faster!
+          </span>
+          <div className={subtitle({ class: "mt-4" })}>
+          Get instant AI-powered feedback, optimize your resume, and boost
+          your chances of landing your dream job." 🚀
+          </div>
+        </div>
+
+        <div className="flex gap-3">
+          <Link
+            isExternal
+            className={buttonStyles({
+              color: "primary",
+              radius: "full",
+              variant: "shadow",
+            })}
+            href={siteConfig.links.docs}
+          >
+            Documentation
+          </Link>
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.links.github}
+          >
+            <GithubIcon size={20} />
+            GitHub
+          </Link>
+        </div>
+
+        <div className="mt-8">
+          <Snippet hideCopyButton hideSymbol variant="bordered">
+            <span>
+              Upload Your Resume{" "}
+              <Code color="primary">Sign In</Code>
+            </span>
+          </Snippet>
+        </div>
+      </section>
+    </DefaultLayout>
+  );
+}
